b51d377b7b79ed443728273078a0b2eb42705309
Alfadas <alfadas.3@gmail.com>
2019-10-05 13:01:24 +0200

    basic Plants & prefab base

---
+++ Assets/Scripts/Plants/PlantPropertys.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlantPropertys
+{
+    [SerializeField] int eatable;
+    [SerializeField] int stable;
+    public PlantPropertys(int eatable, int stable)
+    {
+        this.eatable = eatable;
+        this.stable = stable;
+    }
+    public int Eatable
+    {
+        get
+        {
+            return eatable;
+        }
+    }
+    public int Stable
+    {
+        get
+        {
+            return stable;
+        }
+    }
+}
