86b29a03a438bfda48f42a54d01a327a1ff9f3c3
Alfadas <alfadas.3@gmail.com>
2019-10-05 21:22:13 +0200

    merge fix

--- Assets/Scripts/Plants/PlantPropertys.cs
+++ Assets/Scripts/Plants/PlantPropertys.cs
@@ -6,10 +6,6 @@ public struct PlantPropertys
 {
     [SerializeField] int eatable;
     [SerializeField] int stable;
-<<<<<<< HEAD
-=======
-
->>>>>>> e654c81d1cd84bdf8c1ab739cfde20684654a8ff
     public PlantPropertys(int eatable, int stable)
     {
         this.eatable = eatable;
