d5fbd7016525f6c81a1aaed6ba060ce8f0064ef3
Alfadas <alfadas.3@gmail.com>
2019-10-06 20:45:15 +0200

    mergefix, removed testmat

--- Assets/Scripts/Tiles/Tile.cs
+++ Assets/Scripts/Tiles/Tile.cs
@@ -5,14 +5,10 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] public int water = 1;
     [SerializeField] public int fertility = 1;
-<<<<<<< HEAD
     [SerializeField] private TileView tileViewPrefab;
     public Plant Plant { get; set; }
     public GlobalInformation global { get; set; }
-=======
-    private Plant Plant { get; set; }
-    private GlobalInformation global { get; set; }
->>>>>>> 9280cd38ce5e5e50be329cf7f6bafc66b5273000
+
 
     private void Awake()
     {
