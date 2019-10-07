6c47fee20c7f8d392b3fce0593c3a29f58437aaa
Alfadas <alfadas.3@gmail.com>
2019-10-07 14:07:15 +0200

    outcommented testLog

--- Assets/Scripts/Plants/Plant.cs
+++ Assets/Scripts/Plants/Plant.cs
@@ -122,7 +122,7 @@ public class Plant : MonoBehaviour
 
     public void ResistLocalWind(int localWind)
     {
-        Debug.Log(WindResistance * 2 - localWind);
+        //Debug.Log(WindResistance * 2 - localWind);
     }
     void ChangeHealth(int change)
     {
